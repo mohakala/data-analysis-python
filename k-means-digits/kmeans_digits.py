493f740f7968593524526b3741707eaffd561198
mikko.o.hakala@gmail.com <mikko.o.hakala@gmail.com>
2016-05-21 13:17:43 +0300

    first commits

--- k-means-digits/kmeans_digits.py
+++ k-means-digits/kmeans_digits.py
@@ -7,7 +7,6 @@ import os
 
 
 # Fetch data
-
 if (not os.path.exists("/Python34/test_mnist/mldata")):
     mnist = fetch_mldata('MNIST original', data_home='test_mnist')
 else:
@@ -17,7 +16,6 @@ print('unique targets:',np.unique(mnist.target))
 
 
 # Select the training set
-
 mnist.data, mnist.target = shuffle(mnist.data,mnist.target)
 mnist2=mnist.data[-5000:]
 print(type(mnist2))
@@ -25,11 +23,36 @@ print(mnist2.shape)
 
 
 # Plot
-
 plt.rc("image", cmap="binary")
-plt.imshow(mnist2[0].reshape(28,28))
+
+# plt.imshow(mnist2[0].reshape(28,28))
+# plt.show()
+
+for i in range(10):
+    plt.subplot(2,5,i+1)
+    plt.imshow(mnist2[i].reshape(28,28))
+    plt.xticks(())
+    plt.yticks(())
+plt.tight_layout()
 plt.show()
-plt.imshow(mnist2[1].reshape(28,28))
+
+
+# Use K-Means
+kmeans = KMeans(20)
+mu_digits = kmeans.fit(mnist2).cluster_centers_
+
+# KESKEN
+
+plt.figure(figsize=(16,6))
+for i in range(2*(mu_digits.shape[0]/2)): # loop over all means
+    plt.subplot(2,mu_digits.shape[0]/2,i+1)
+    plt.imshow(mu_digits[i].reshape(28,28))
+    plt.xticks(())
+    plt.yticks(())
+plt.tight_layout()
 plt.show()
+
+
+
 
 
