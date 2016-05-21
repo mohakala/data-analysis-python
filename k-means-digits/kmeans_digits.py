42eb1c798fee2efc7417452b1468df3e968d06bc
mikko.o.hakala@gmail.com <mikko.o.hakala@gmail.com>
2016-05-21 21:34:43 +0300

    code works

--- k-means-digits/kmeans_digits.py
+++ k-means-digits/kmeans_digits.py
@@ -5,6 +5,20 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
+"""
+
+K-Means 
+Unsupervised learning of images (hand-written digits)
+Following the example from Python Notebook:
+http://nbviewer.jupyter.org/github/temporaer/tutorial_ml_gkbionics/tree/master/
+
+"""
+
+
+# Set the size of the training set and the number of clusters 
+nTrainingSet=10000
+nClusters=20
+
 
 # Fetch data
 if (not os.path.exists("/Python34/test_mnist/mldata")):
@@ -17,7 +31,8 @@ print('unique targets:',np.unique(mnist.target))
 
 # Select the training set
 mnist.data, mnist.target = shuffle(mnist.data,mnist.target)
-mnist2=mnist.data[-5000:]
+mnist2=mnist.data[-nTrainingSet:]
+label2=mnist.target[-nTrainingSet:]
 print(type(mnist2))
 print(mnist2.shape)
 
@@ -31,43 +46,74 @@ plt.rc("image", cmap="binary")
 for i in range(10):
     plt.subplot(2,5,i+1)
     plt.imshow(mnist2[i].reshape(28,28))
+    plt.title(label2[i])
     plt.xticks(())
     plt.yticks(())
 plt.tight_layout()
-plt.show()
-
-
-# Choose and show data to be predicted
-sample=mnist2[2000]
-plt.imshow(sample.reshape(28,28))
-plt.title('Sample to be predicted')
+plt.suptitle("Examples from the dataset", size=16)
 plt.show()
 
 
 
 # Use K-Means
-kmeans = KMeans(20)
-mu_digits = kmeans.fit(mnist2).cluster_centers_
-mu_lab=kmeans.fit(mnist2).labels_
+kmeans = KMeans(nClusters)
+mu=kmeans.fit(mnist2)
+mu_digits=mu.cluster_centers_
+mu_lab=mu.labels_
+print('mu_digits.shape:',mu_digits.shape)
+
+
+
+
+# Choose and show data to be predicted
+sampleIndex=int(nTrainingSet/2) # Take smpl from middle ofset
+sample=mnist2[sampleIndex]
+plt.imshow(sample.reshape(28,28))
+plt.title('Sample to be predicted:'+str(label2[sampleIndex]))
+plt.show()
+print('Sample to be predicted: '+str(label2[sampleIndex]))
 
 
 # Prediction based on clusters
 prediction=kmeans.predict(sample)
-print('Prediction: label = ', prediction) 
+print('Prediction: Cluster label = ', prediction) 
 
 
-# Show 
-plt.figure(figsize=(16,6))
-for i in range(int(2*(mu_digits.shape[0]/2))): # loop over all means
-    plt.subplot(2,mu_digits.shape[0]/2,i+1)
-    plt.imshow(mu_digits[i].reshape(28,28))
-    plt.title(mu_lab[i])
-    plt.xticks(())
-    plt.yticks(())
+# Plot examples belonging to cluster 'prediction'
+cluster=prediction
+i=0
+for ii in range(400):
+    if (mu_lab[ii]==cluster):
+        plt.subplot(2,6,i+1)
+        plt.imshow(mnist2[ii].reshape(28,28))
+        plt.xticks(())
+        plt.yticks(())
+        i+=1
+        if (i>10):
+            break
 plt.tight_layout()
+plt.suptitle("The sample belongs to cluster "+str(prediction)+", example cases:", size=16)
 plt.show()
 
-# TO DO MORE: doesn't work yet
+
+# Show clusters and the sample to be predicted
+print('Plotting the clusters that were found')
+plt.figure(figsize=(16,6))
+for i in range(int(2*(mu_digits.shape[0]/2))): # loop over all means
+    plt.subplot(2,mu_digits.shape[0]/2+1,i+1)
+    plt.imshow(mu_digits[i].reshape(28,28))
+    plt.title(i)
+    plt.xticks(())
+    plt.yticks(())
+plt.subplot(2,mu_digits.shape[0]/2+1,i+2)
+plt.imshow(sample.reshape(28,28))
+plt.title('Predicted label:'+str(prediction))
+plt.xticks(())
+plt.yticks(())
+plt.tight_layout()
+plt.suptitle("The clusters that were found", size=16)
+plt.show()
+
 
 
 
