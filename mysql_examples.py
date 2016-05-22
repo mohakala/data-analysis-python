17699e175ed611daea7a484b912e0d95f2a11aee
mikko.o.hakala@gmail.com <mikko.o.hakala@gmail.com>
2016-05-22 13:06:04 +0300

    added dbAction and more tests

--- mysql_examples.py
+++ mysql_examples.py
@@ -5,6 +5,22 @@ http://www.tutorialspoint.com/python/python_database_access.htm
 http://www.w3schools.com/sql/sql_syntax.asp
 """
 
+def dbAction(sql):
+   data=[]
+   try:
+      # Execute SQL command
+      cursor.execute(sql)
+      db.commit()
+      data = cursor.fetchall()
+      print('Success executing SQL command') 
+   except:
+      # Rollback in case there is any error
+      db.rollback()
+      print('Not success executing SQL command') 
+   return(data)
+
+
+
 # Open database connection
 db = MySQLdb.connect("localhost","testuser","test123","TESTDB" )
 
@@ -16,12 +32,12 @@ cursor.execute("SELECT VERSION()")
 
 # Fetch a single row using fetchone() method.
 data = cursor.fetchone()
-
 print ("Database version : %s " % data)
 
 
-# COMMANDS
 
+
+# COMMANDS
 
 """
     SELECT - extracts data from a database
@@ -37,40 +53,40 @@ print ("Database version : %s " % data)
     DROP INDEX - deletes an index
 """
 
+
 # show databases
 # use testdb
 # select * from employee
+# select FIRST_NAME, LAST_NAME from employee;
+
 
 # INSERT
-sql = "INSERT INTO EMPLOYEE(FIRST_NAME, \
+sql = "xINSERT INTO EMPLOYEE(FIRST_NAME, \
        LAST_NAME, AGE, SEX, INCOME) \
        VALUES ('%s', '%s', '%d', '%c', '%d' )" % \
-       ('Patty', 'Miller', 35, 'F', 4000)
-
-try:
-   # Execute the SQL command
-   cursor.execute(sql)
-   # Commit your changes in the database
-   db.commit()
-   print('Success executing SQL command') 
-except:
-   # Rollback in case there is any error
-   db.rollback()
-   print('Not success executing SQL command') 
-
+       ('Tom', 'Johnson', 56, 'F', 4000)
+data=dbAction(sql)
 
 # SELECT
 sql = "SELECT * FROM EMPLOYEE" 
+data=dbAction(sql)
+print("Data fetched:\n",data)
 
-try:
-   cursor.execute(sql)
-   data = cursor.fetchall() 
-   print("Data fetched:\n",data)
-   print('Success executing SQL command') 
-except:
-   # Rollback in case there is any error
-   db.rollback()
-   print('Not success executing SQL command') 
+# SELECT DISTINCT City FROM Customers;
+sql = "SELECT DISTINCT FIRST_NAME FROM EMPLOYEE"
+data=dbAction(sql)
+print("Data fetched:\n",data)
+
+# SELECT * FROM Customers WHERE Country='Mexico';
+# http://www.w3schools.com/sql/sql_where.asp
+sql = "SELECT INCOME FROM EMPLOYEE WHERE INCOME>2000"
+data=dbAction(sql)
+print("Data fetched:\n",data)
+
+sql = "SELECT FIRST_NAME FROM EMPLOYEE WHERE FIRST_NAME LIKE '%o%'"
+data=dbAction(sql)
+print("Data fetched:\n",data)
+
 
 
 
