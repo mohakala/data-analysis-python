d81a8a09864f6e9f09e32b548547042f2dca4643
mikko.o.hakala@gmail.com <mikko.o.hakala@gmail.com>
2016-08-01 21:52:21 +0300

    se

--- se/se.py
+++ se/se.py
@@ -2,14 +2,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
+# Read the raw data
 rawdata='../../datasets/sedat.xls'
 df = pd.read_excel(rawdata)
+
+
+
+# Columns
 
 ### not needed
 ### df.columns = ['x', 'ref','c01','c05','c1','c15','c5','c10']
 
 df = df.rename(columns={'Wavelength': 'wl'})
-
 x=df['wl'].values
 ref=df[0].values
 
@@ -21,12 +25,40 @@ ref=df[0].values
 #are=[0 for i in range(5)]
 
 ar=[]
-for i in df.columns[1:]:
+for i in df.columns[3:]:
     area=np.trapz( np.abs(df[i]-ref)    ,dx=2)
     print(i)
     ar.append(area)
 
-conc=df.columns[1:].values
+conc=df.columns[3:].values  # object
+concNumeric=pd.to_numeric(conc)
+
+
+# Make a linear model
+
+m,b=np.polyfit(concNumeric,ar,1) # linear
+print('m,b=',m,b)
+
+
+
+# Predictions
+
+testSpectrum=0.3*df[0.5]+0.7*df[1.5]
+area = np.trapz( np.abs(testSpectrum-ref)    ,dx=2)
+predConc=(area-b)/m
+print('predicted:',predConc,' calc:',0.3*0.5+0.7*1.5)
+
+testSpectrum=0.3*df[0.5]+0.7*df[5]
+area = np.trapz( np.abs(testSpectrum-ref)    ,dx=2)
+predConc=(area-b)/m
+print('predicted:',predConc,' calc:',0.3*0.5+0.7*5)
+
+testSpectrum=1.0*df[0.5]
+area = np.trapz( np.abs(testSpectrum-ref)    ,dx=2)
+predConc=(area-b)/m
+print('predicted:',predConc,' calc:',1.0*0.5)
+
+
 
 
 
@@ -41,7 +73,11 @@ ax=fig.add_subplot(2,2,2)
 ax.plot(x,df[0.1]-ref,'-',x,df[1]-ref,'-',x,df[0.5]-ref,'-')
 
 ax=fig.add_subplot(2,2,3)
-ax.plot(conc,ar,'o')
+ax.plot(conc,ar,'o',conc,m*conc+b,'k-')
+
+ax=fig.add_subplot(2,2,4)
+ax.plot(x,testSpectrum-ref,'-')
+
 
 
 plt.show()
