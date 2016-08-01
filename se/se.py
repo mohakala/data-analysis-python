e85a38e5478237c0ff96290ba3cc878ed00e45f2
mikko.o.hakala@gmail.com <mikko.o.hakala@gmail.com>
2016-08-01 23:11:32 +0300

    sensitivity tests

--- se/se.py
+++ se/se.py
@@ -29,15 +29,24 @@ for i in df.columns[3:]:
     area=np.trapz( np.abs(df[i]-ref)    ,dx=2)
     print(i)
     ar.append(area)
-
 conc=df.columns[3:].values  # object
 concNumeric=pd.to_numeric(conc)
 
 
-# Make a linear model
+# Make a linear model for c=0.5 and above
 
 m,b=np.polyfit(concNumeric,ar,1) # linear
 print('m,b=',m,b)
+
+
+arAll=[]  # All values
+for i in df.columns[1:]:
+    area=np.trapz( np.abs(df[i]-ref)    ,dx=2)
+    print(i)
+    arAll.append(area)
+concAll=df.columns[1:].values  # object
+
+
 
 
 
@@ -59,6 +68,72 @@ predConc=(area-b)/m
 print('predicted:',predConc,' calc:',1.0*0.5)
 
 
+# How well the known datapoints are predicted?
+
+print(' ')
+for i in range(5):
+    predConc=(ar[i]-b)/m
+    print('predicted:',predConc,' conc:',conc[i])
+    
+
+
+
+
+# Make a linear model with 4 datapoints out of 5
+# Check then how well the known one is predicted
+
+print(' ')
+concError=[]
+
+points=(1,2,3,4)
+missing=0
+aux1=list( concNumeric[i] for i in points )
+aux2=list( ar[i]          for i in points )
+mm,bb=np.polyfit(aux1,aux2,1) # linear
+predConc=(ar[missing]-bb)/mm
+print('predicted:',predConc,' known:',conc[missing])
+concError.append(predConc-conc[missing])
+
+points=(0,2,3,4)
+missing=1
+aux1=list( concNumeric[i] for i in points )
+aux2=list( ar[i]          for i in points )
+mm,bb=np.polyfit(aux1,aux2,1) # linear
+predConc=(ar[missing]-bb)/mm
+print('predicted:',predConc,' known:',conc[missing])
+concError.append(predConc-conc[missing])
+
+points=(0,1,3,4)
+missing=2
+aux1=list( concNumeric[i] for i in points )
+aux2=list( ar[i]          for i in points )
+mm,bb=np.polyfit(aux1,aux2,1) # linear
+predConc=(ar[missing]-bb)/mm
+print('predicted:',predConc,' known:',conc[missing])
+concError.append(predConc-conc[missing])
+
+points=(0,1,2,4)
+missing=3
+aux1=list( concNumeric[i] for i in points )
+aux2=list( ar[i]          for i in points )
+mm,bb=np.polyfit(aux1,aux2,1) # linear
+predConc=(ar[missing]-bb)/mm
+print('predicted:',predConc,' known:',conc[missing])
+concError.append(predConc-conc[missing])
+
+points=(0,1,2,3)
+missing=4
+aux1=list( concNumeric[i] for i in points )
+aux2=list( ar[i]          for i in points )
+mm,bb=np.polyfit(aux1,aux2,1) # linear
+predConc=(ar[missing]-bb)/mm
+print('predicted:',predConc,' known:',conc[missing])
+concError.append(predConc-conc[missing])
+
+print('concError:',concError)
+
+
+
 
 
 
@@ -67,16 +142,20 @@ print('predicted:',predConc,' calc:',1.0*0.5)
 fig=plt.figure()
 
 ax=fig.add_subplot(2,2,1)
-ax.plot(x,df[0],'-',x,df[10],'--')
+ax.plot(x,df[0.1]-ref,'-',x,df[0.5]-ref,'-',x,df[1]-ref,'-',x,df[1.5]-ref,'-',x,df[5]-ref,'-',x,df[10]-ref,'-')
+ax.set_title("Changes in spectra wrt. 0-spectrum")
+ax.axes.get_xaxis().set_ticks([])
+ax.set_xlabel('Wavelength')
 
 ax=fig.add_subplot(2,2,2)
-ax.plot(x,df[0.1]-ref,'-',x,df[1]-ref,'-',x,df[0.5]-ref,'-')
+ax.plot(concAll,arAll,'o')
+#ax.set_xlim(0,10)
+#ax.set_ylim(0,60000)
+ax.set_title("Absolute area difference wrt. 0-spectrum")
 
 ax=fig.add_subplot(2,2,3)
 ax.plot(conc,ar,'o',conc,m*conc+b,'k-')
-
-ax=fig.add_subplot(2,2,4)
-ax.plot(x,testSpectrum-ref,'-')
+ax.set_title("Linear fit for concentrations c=0.5 ... 10")
 
 
 
