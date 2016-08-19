108fd1add17b9f160b429acf90272ba819a7e562
mikko.o.hakala@gmail.com <mikko.o.hakala@gmail.com>
2016-08-20 00:10:46 +0300

    added Wilcoxon and Mann-Whitney

--- mfunc.py
+++ mfunc.py
@@ -4,6 +4,46 @@ import statsmodels.api as sm
 from scipy.stats import linregress
 
 
+def quickStudy2(x,y):
+    # Descriptive statistics and linear regrsssion
+    print('MEAN X = ',x.mean(),'+_',np.std(x, ddof=1)/np.sqrt(len(x)))
+    print('MEAN Y = ',y.mean(),'+_',np.std(y, ddof=1)/np.sqrt(len(x)))
+    print('STD Y = ',np.std(y, ddof=1))
+    slope, intercept, r, prob2, see = linregress(x, y)
+    # see = standard errof of estimated gradient
+    print('Pearson correlation coefficient r:',r)
+    print('R^2 coefficient of determination (Selitysaste):',r**2)
+    print('P-VALUE (2-SIDED) FOR SLOPE TO BE ZERO =',prob2)
+    print('slope,intercept=',slope,intercept)
+    m,b=np.polyfit(x,y,1) 
+    print('m,b=',m,b)
+
+    # Ordinary linear system for x,y
+    xx= sm.add_constant(x,prepend=False)
+    results=sm.OLS(y,xx).fit()
+    print(results.summary())
+
+    # Ordinary linear system for x,x^2,y
+    xx= sm.add_constant(x,prepend=False)
+    x2=x[:,np.newaxis]*x[:,np.newaxis] # second power
+    xxx = np.concatenate((xx,x2),axis=1)
+    results=sm.OLS(y,xxx).fit()
+
+def wilcoxon(x,y):
+    # Wilcoxon rank-sum test for pairs of samples
+    # Are the samples from different or same distribution?
+    import scipy
+    z_stat, p_val = scipy.stats.ranksums(x,y)  
+    print('P-value of ranksumtest=',p_val)
+
+def mannWhitney(x,y):
+    # Mann-Whitney
+    # Are the samples from different or same distribution?
+    import scipy
+    z_stat, p_val = scipy.stats.mannwhitneyu(x, y, 'two-sided')
+    print('P-value of Mann-Whitney=',p_val)
+    
+
 def plotExamples(x,y):
 # plot examples 
     import numpy as np
@@ -65,10 +105,15 @@ def pcntval(val,ref):
 
 if __name__ == '__main__':
     print('Main of mfunc.py')
-    x=np.array([1,2,3])
-    y=np.array([1,5,10])
-    plotExamples(x,y)
+    # x=np.array([1,2,3])
+    # y=np.array([1,5,10])
+    x=np.arange(8)
+    y=np.array([0,1,3,4,5,8,9,10])
 
+    # plotExamples(x,y)
+    quickStudy2(x,y)
+    wilcoxon(x,y)
+    mannWhitney(x,y)
 
 
 
