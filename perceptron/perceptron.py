9c7d36e572923804fea06fd7b8c89bb3b63922b3
mikko.o.hakala@gmail.com <mikko.o.hakala@gmail.com>
2016-08-21 01:39:20 +0300

    first steps done

--- perceptron/perceptron.py
+++ perceptron/perceptron.py
@@ -10,15 +10,35 @@ from mfunc import *  # Some own functions
 # m inputs
 # n neurons
 
+def heaviside(x):
+    # for an array returns array th:
+    # th_i=1 if x_i > 0, otherwise th_i=0 
+    th=(np.sign(x)+1)/2
+    # must repair the value th=0.5 corresponding to x=0
+    th[th==0.5] = 0
+    return(th)
+
+def test_heaviside(): # call from main
+    test=np.array([-1,-0.01,0,0.02,1])
+    print('test heaviside:',test)
+    print(heaviside(test))        
+
+
 def runPerceptron(inp,w):
-    m=len(w) # inputs
-    n=len(w[0]) # neurons
+    # inp: column vector, length: m
+    # w: weight matrix m x n
+    # returns: values of the n neurons 0/1 
+    print('runPerceptron')
+    m=len(w) # number of inputs (rows of w)
+    n=len(w[0]) # neurons (columns of w)
     m2=len(inp) # inputs (alternative)
     print('Inputs:',m2,'Inputs from w:',m,'Neurons:',n)
-    # return(out)
+    out=np.mat(w.T) * np.mat(inp) # matrix product to get neurons' outputs
+    print('Aux output:',out)
+    return(heaviside(out))
     
 
-def trainPerceptron(inp,out,target):
+def trainPerceptron(w,inp,out,target):
     pass
     # set random values for w
     # 
@@ -26,11 +46,18 @@ def trainPerceptron(inp,out,target):
 
 
 if __name__ == '__main__':
-    x=np.array([1,0])
+    inp=np.array([[-1,0,0]]).T
+    print('Input:',inp)
 
-#    w=np.array([[1,0],[2,0],[1,2]]).T
-    w=(np.random.rand(2,3)-+0.5)*0.1
-    runPerceptron(x,w)
+    example=np.array([[1,0],[2,0],[1,2]]).T  # 2x3 matrix
+
+#    w=(np.random.rand(3,1)-0.5)*0.1 # initial random weights
+    w=np.array([[-0.05,-0.02,0.02]]).T
+
+    out=runPerceptron(inp,w)
+    print('Output:',out)
+
+
 
 
 
