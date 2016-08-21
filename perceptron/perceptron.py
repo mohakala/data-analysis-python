56afcd8a3945883ef05958d183b588aa60d5b2b9
mikko.o.hakala@gmail.com <mikko.o.hakala@gmail.com>
2016-08-21 20:13:39 +0300

    finished one neuron case, working

--- perceptron/perceptron.py
+++ perceptron/perceptron.py
@@ -8,9 +8,10 @@ sys.path.insert(0, 'C:\Python34\data-analysis-python')
 from mfunc import *  # Some own functions
 sys.path.insert(0, 'C:\\Python34\\data-analysis-python\\random-forest-python')
 from rf_titanic import *  # For confusion matrix
-
 # print(sys.path)
 
+
+# TODO: More than one neuron
 
 # m inputs
 # n neurons
@@ -23,7 +24,7 @@ def heaviside(x):
     th[th==0.5] = 0
     return(th)
 
-def test_heaviside(): # call from main
+def test_heaviside(): # (call from main)
     test=np.array([-1,-0.01,0,0.02,1])
     print('test heaviside:',test)
     print(heaviside(test))        
@@ -33,52 +34,77 @@ def runPerceptron(inp,w):
     # inp: column vector, length: m
     # w: weight matrix m x n
     # returns: values of the n neurons 0/1 
-    print('runPerceptron')
-    m=len(w) # number of inputs (rows of w)
-    n=len(w[0]) # neurons (columns of w)
-    m2=len(inp) # inputs (alternative)
-    print('Inputs:',m2,'Inputs from w:',m,'Neurons:',n)
+    # print('runPerceptron')
+
+    #print('Inputs:',m2,'Inputs from w:',m,'Neurons:',n)
     out=np.mat(w.T) * np.mat(inp) # matrix product to get neurons' outputs
-    print('Aux output:',out)
+    #print('Aux output:',out)
     return(heaviside(out))
     
 
-def trainPerceptron(w,inp,out,target,eta):
+def trainPerceptronOneRound(w,inp,target,eta):
+    # One round for all input datavectors
+    changeForInputSet=0
+    nInputs=len(inp[0])
+    for i_inp in range(nInputs):
+        #print('Input datavector:',i_inp)
+        oneInput=inp[:,i_inp].reshape((3,1))
+        #print('one input:',oneInput)
+        out=runPerceptron(oneInput,w)
+        for i in range(3): # update weights of neurons
+            #print('Updating neuron:',i)
+            change=eta*(target[i_inp]-out)*oneInput[i]
+            w[i]=w[i]+change
+            changeForInputSet+=np.absolute(change)
+    print('Change for input set =',changeForInputSet)
+    return(w,changeForInputSet)
 
-    # TODO Loop over inputs
-    # TODO Convergence criterion
-    
-    out=runPerceptron(inp,w)
 
-    for i in range(3): # update weights
-        w[i]=w[i]+eta*(target-out)*inp[i]
-    # print(w) 
-
-    out=runPerceptron(inp,w)
-
+def trainPerceptron(w,inp,target,eta):
+    i=-1
+    while True:
+        i+=1
+        print('Iteration:',i)
+        w,change = trainPerceptronOneRound(w,inp,target,eta)
+        if (change==0):
+            break
     return(w)
 
 
 if __name__ == '__main__':
-    inp=np.array([[-1,0,0]]).T
-    target=np.array([0])
-    eta=0.25 # learning rate
-    print('Input:',inp,'Target:',target,'Eta:',eta)
-    
+    # Parameters and inputs
+    inp=np.array([[-1,0,0],[-1,0,1],[-1,1,0],[-1,1,1]]).T
     # example=np.array([[1,0],[2,0],[1,2]]).T  # 2x3 matrix
-
-    # Run perceptron once
+    target=np.array([0,1,1,1])
+    eta=0.25 # learning rate
     # w=(np.random.rand(3,1)-0.5)*0.1 # initial random weights
     w=np.array([[-0.05,-0.02,0.02]]).T
-    out=runPerceptron(inp,w)
-    print('Output:',out)
 
-    trainPerceptron(w,inp,out,target,eta)
+    # Print information
+    print('System:')
+    print('Input:\n',inp,'\nTarget:',target,'Eta:',eta)
+    m=len(inp[0]) # number of inputs (rows of w)
+    n=len(w[0])   # neurons (columns of w)
+    print('Number of inputs:',m,' neurons:',n)
+    print('Initial weights',w)
+
+    # Train perceptron
+    print('-- train perceptron:')
+    trainPerceptron(w,inp,target,eta)
+    print('Final weights:',w)
+
+    # Check perceptron for the input data
+    print('Check that the perceptron works:')
+    for i in range(4):
+        print('Input:\n',inp[:,i])
+        print('Output:',runPerceptron(inp[:,i].reshape((3,1)),w))
+
+    # Another test
+    testinput=np.array([-1,0.3,0.3]).reshape(3,1)
+    print('Another test, testinput:\n',testinput,'\n Output:\n',runPerceptron(testinput,w))
 
 
-
-
-#   Just tests
+#   dump, just tests
     aaatrue=np.array([0,1,0,1])
     bbbpred=np.array([1,1,1,0])
 
