b7c11aae43177809795e0949765d3f11a9f5320a
mikko.o.hakala@gmail.com <mikko.o.hakala@gmail.com>
2017-02-06 23:29:29 +0200

    added cross-validation score

--- naive_bayes/nb.py
+++ naive_bayes/nb.py
@@ -1,5 +1,6 @@
-
 #https://www.analyticsvidhya.com/blog/2015/09/naive-bayes-explained/
+
+print('--- Testing the basics ---')
 
 #Import Library of Gaussian Naive Bayes model
 from sklearn.naive_bayes import GaussianNB
@@ -20,7 +21,7 @@ predicted= model.predict([[1,2],[3,4]])
 print(predicted)
 
 
-
+# Quick way to get training and testing sets 
 #import pandas as pd
 #person = pd.read_csv(‘example.csv’)
 #mask = np.random.rand(len(sales)) < 0.8
@@ -28,7 +29,11 @@ print(predicted)
 #test = sales[~mask]
 
 
+##########################################
 # Testing Naive Beyes for the Iris dataset
+##########################################
+
+print('--- Testing for the Iris dataset ---')
 
 import pandas as pd
 
@@ -36,13 +41,12 @@ def exploreData(df):
     print(df.head(5))
     print(df.describe())
     print("Data types in df:\n",df.dtypes)
-    #print(df[0].value_counts())
 
-# Get data
-# - no header
+# Get data (datafile has no header)
 df = pd.read_csv('../../datasets/iris.data',header=None)
 
-# Data munging: categorize the text
+# Data munging: categorize the text 
+# https://www.continuum.io/content/pandas-categoricals
 df[4] = df[4].astype('category')
 
 # Explore
@@ -59,34 +63,41 @@ print('Test set:\n',test.head(3))
 Xtrain=train[[0,1,2,3]].as_matrix()
 ytrain=train[[4]].values.ravel()
 
-#Xtrain=train[[0,1,2,3]]
-#ytrain=train[[4]]
-
-
-#xt=Xtrain.as_matrix()
-
 Xtest=test[[0,1,2,3]].as_matrix()
 ytest=test[[4]].values
 
-
-
 # Train 
-
-#Create a Gaussian Classifier
-model = GaussianNB()
-
-#Train the model using the training sets 
+model = GaussianNB() #Create a Gaussian Classifier
 model.fit(Xtrain, ytrain)
 
-# Test 
 
-model.predict(Xtest[0])
+# Test 
+correct=0
 for i in range(len(ytest)):
     predicted= model.predict(Xtest[i])
     print('pred, true:',predicted,ytest[i])
+    if(predicted==ytest[i]): correct+=1
+print('Correct predictions:',correct/len(ytest)*100.0,'%')
+
+print('Other tetss, predict_proba (probability to belong to a class):')
+print(model.predict_proba(Xtest[0]))
+print('Categories:',df[4].cat.categories)
+
+# Cross-validation
+# https://www.analyticsvidhya.com/blog/2015/11/improve-model-performance-cross-validation-in-python-r/
+# from sklearn.model_selection import cross_val_score
+from sklearn.cross_validation import cross_val_score
+n_folds = 10
+score=cross_val_score(model, Xtrain, ytrain, cv=n_folds, n_jobs=1)
+print('n_folds, average cross-validation score:',n_folds, score.mean())
 
 
 
+##########################################
+# Naive Beyes articles
+##########################################
+
+# https://www.quora.com/What-types-of-data-sets-are-appropriate-for-Naive-Bayes
 
 
 
