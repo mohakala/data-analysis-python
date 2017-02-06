6cec89d6181ffa709e006561068fa0fa1d5d165c
mikko.o.hakala@gmail.com <mikko.o.hakala@gmail.com>
2017-02-06 21:49:59 +0200

    predictions

--- naive_bayes/nb.py
+++ naive_bayes/nb.py
@@ -28,22 +28,64 @@ print(predicted)
 #test = sales[~mask]
 
 
-# Iris dataset
+# Testing Naive Beyes for the Iris dataset
 
 import pandas as pd
 
-def getData(rawdata):
-    df = pd.read_csv(rawdata) 
-    return df
-
 def exploreData(df):
-    print(df.head(16))
+    print(df.head(5))
     print(df.describe())
     print("Data types in df:\n",df.dtypes)
-    print('Frequency distributions:')
+    #print(df[0].value_counts())
 
-df = getData('../../datasets/iris.data')
+# Get data
+# - no header
+df = pd.read_csv('../../datasets/iris.data',header=None)
+
+# Data munging: categorize the text
+df[4] = df[4].astype('category')
+
+# Explore
 exploreData(df)
+print('Categories:',df[4].cat.categories)
+
+# Select training and testing sets
+mask = np.random.rand(len(df)) < 0.8
+train = df[mask]
+test = df[~mask]
+print('Sizes, train, test:',len(train),len(test))
+print('Test set:\n',test.head(3))
+
+Xtrain=train[[0,1,2,3]].as_matrix()
+ytrain=train[[4]].values.ravel()
+
+#Xtrain=train[[0,1,2,3]]
+#ytrain=train[[4]]
+
+
+#xt=Xtrain.as_matrix()
+
+Xtest=test[[0,1,2,3]].as_matrix()
+ytest=test[[4]].values
+
+
+
+# Train 
+
+#Create a Gaussian Classifier
+model = GaussianNB()
+
+#Train the model using the training sets 
+model.fit(Xtrain, ytrain)
+
+# Test 
+
+model.predict(Xtest[0])
+for i in range(len(ytest)):
+    predicted= model.predict(Xtest[i])
+    print('pred, true:',predicted,ytest[i])
+
+
 
 
 
