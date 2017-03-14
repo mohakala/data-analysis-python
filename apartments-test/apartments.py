e5b1f185a9a8706bbe88ef449f72106b5995b231
mikko.o.hakala@gmail.com <mikko.o.hakala@gmail.com>
2017-03-15 00:40:57 +0200

    clean

--- apartments-test/apartments.py
+++ apartments-test/apartments.py
@@ -68,7 +68,7 @@ def classification_model(model, data, predictors, outcome):
         # Training the algorithm using the predictors and target.
         model.fit(train_predictors, train_target)
     
-        #Record error from each cross-validation run
+        # Record error from each cross-validation run
         error.append(model.score(data[predictors].iloc[test,:], data[outcome].iloc[test]))
  
     print("Cross-Validation Score : %s" % "{0:.3%}".format(np.mean(error)))
@@ -77,22 +77,16 @@ def classification_model(model, data, predictors, outcome):
     model.fit(data[predictors],data[outcome]) 
 
 
-if __name__ == '__main__':
-    lin()
-
-    # A Get the data
-    rawdata='../../datasets/nurmijarvi_asunnot_250316.csv'
-    #rawdata='nurmijarvi_asunnot_250316.csv'
-    df=getData(rawdata)
-    print("raw data dataframe size:",df.shape)
+def exploreData(df):
+    print(df.head(5))
+    print(df.describe())
+    print("Data types in df:\n",df.dtypes)
 
 
+def exploratory(df):
     # B Exploratory analysis    
+
     # B.1 Quick data exploration
-    def exploreData(df):
-        print(df.head(5))
-        print(df.describe())
-        print("Data types in df:\n",df.dtypes)
 
     lin()
     exploreData(df)
@@ -101,6 +95,10 @@ if __name__ == '__main__':
     print(df['Kaupunginosa'].value_counts())
     print(df['Talotiedot'].value_counts())
     print(df['Kunto'].value_counts())
+
+
+    # input("\nPress Enter to continue")
+    print('-----------\n')
 
 
     # B.2 Distribution analysis
@@ -117,6 +115,7 @@ if __name__ == '__main__':
         plt.show()
 
     if(False):
+        fig=plt.figure()
         df['Vh'].hist(bins=20)
         plt.title('Vh')
         plt.show()
@@ -132,52 +131,64 @@ if __name__ == '__main__':
         temp3 = pd.crosstab([df['Kunto'], df['Talotiedot']], df['Huoneet'] )
         temp3.plot(kind='bar', stacked=True, color=['red','blue','green','cyan','yellow','black'], grid=False)
         plt.show()
-    
+
+
+def dataMunging(df):
     # C Data munging
+    # Various data munging tasks. Fill missing values etc.
 
     # C.1 Missing values in dataset
     lin()
     missValues=df.apply(lambda x: sum(x.isnull()),axis=0)
     print('Missing values in columns:\n',missValues)
 
+
     # C.2 Fill missing values
-    # df['LoanAmount'].fillna(df['LoanAmount'].mean(), inplace=True)
-    # Here simply replace by general 'Nurmijarvi'
-    # Generally this needs to be analyzed thoroughly
-    df['Kaupunginosa'].fillna('Nurmijarvi', inplace=True)
-    # Manual replace
-    # Better ways: learn from http://www.analyticsvidhya.com/blog/2016/03/tutorial-powerful-packages-imputing-missing-values/
+    # See for example http://www.analyticsvidhya.com/blog/2016/03/tutorial-powerful-packages-imputing-missing-values/
+    origCode = False
+    if(origCode):
+        df['LoanAmount'].fillna(df['LoanAmount'].mean(), inplace=True)
+        df['Kaupunginosa'].fillna('Nurmijarvi', inplace=True)
 
-    # C.3 Extreme values (not used)
+    # Fill with the most frequent label
+    df['Kaupunginosa'].fillna(df['Kaupunginosa'].value_counts().index[0], inplace=True)
+
+
+    # C.3 If extreme values, make new columns with log of them (not used)
     df['Vh_log'] = np.log(df['Vh'])
-    df['Vh_log'].hist(bins=20)
-    #plt.show()
+    if(False):
+        fig=plt.figure()
+        df['Vh'].hist(bins=20)
+        plt.title('Vh')
+        plt.show()
+        df['Vh_log'].hist(bins=20)
+        plt.title('Vh, logarithmic')
+        plt.show()
 
-    
-    # D Predictive model
 
-    # D.0 Auxiliary dataframe to encode categorical values to numeric
+    # C.4 Auxiliary dataframe to encode categorical values to numeric
     # in a different way. Replace ok --> xok so the numerical sequence
     # corresponds to kt, rt, xok
     # Added on 4.6.2016
     df_aux=df.copy()
     df_aux['Talotiedot']=df_aux['Talotiedot'].replace({'ok': 'xok'})
-#    sys.exit('Stop')
+
+    if(False):
+        print('Forced stop')
+        sys.exit('Stop')
+
     
-    # D.1 Encode categorical values to numeric
+    # C.5 Encode categorical values to numeric
     var_mod = ['Kaupunginosa','Huoneisto','Talotiedot','Hissi','Kunto']
     le = LabelEncoder()
     for i in var_mod:
         df[i] = le.fit_transform(df[i])
         df_aux[i] = le.fit_transform(df_aux[i])
     print('new df types:\n',df.dtypes) 
-    # what value corresponds to what category?
-    TalotiedotName='kt','ot','rt'
-    TalotiedotName3='kt','rt','xot'
-    # KaupunginosaName='Alppila','Kirkonkylä','Klaukkala',...,'Rajamäki=10'
 
-    # print(df.head(16))
-    
+    TalotiedotName='kt','ot','rt'
+    # KaupunginosaName='Alppila','Kirkonkylä','Klaukkala',...,'Rajamäki=10'
+   
 
     # Write the numerical tables back to file
     if(False):
@@ -196,61 +207,100 @@ if __name__ == '__main__':
         np.savetxt('asunnot_250316_cleaned_numerical3.csv', data_to_file, delimiter=',') 
         # sys.exit('Stop')
 
+    lin()
+    lin()
+    return(df, TalotiedotName)
+
+
+if __name__ == '__main__':
+
+    # A Get the data
+    rawdata='../../datasets/nurmijarvi_asunnot_250316.csv'
+    df=getData(rawdata)
+    print("raw data dataframe size:",df.shape)
+
+    # B Exploratory analysis    
+    exploratory(df)
     
+    # C Data munging
+    df, TalotiedotName = dataMunging(df)
+    # What type corresponds to what numerical category?
+    # TalotiedotName = 'kt','ot','rt'
+    print('TalotiedotName:', TalotiedotName)
 
 
-    #
-    # Task and features
-    #
+    # D Predictive models
+
+    # Task: Määritä talotieto (kt,ot,rt) annetun muuttujan perusteella.
+    outcome_var = 'Talotiedot'
+    print('outcome_var:', outcome_var)
+    lin()
+
     # Features:
     # Kaupunginosa,Huoneet,Talotiedot,
     # m2,Vh,Neliohinta,Rv,Hissi,Kunto
-    #
-    # Task: Määritä talotieto (kt,ot,rt) annetun muuttujan perusteella.
-    #
 
-    outcome_var = 'Talotiedot'
 
     # D.2 Logistic regression
     model = LogisticRegression()
+    print('Model: LogisticRegression')
 
     # D.2.1 Ennuste Talotiedot (kt,ot,rt) <-- Huoneet  
     if(False):
+        fig=plt.figure()
         temp3 = pd.crosstab(df['Huoneet'], df['Talotiedot'])
         temp3.plot(kind='bar', stacked=True, color=['red','blue','green'], grid=False)
         plt.show()
+
     predictor_var = ['Huoneet']
-    classification_model(model, df,predictor_var,outcome_var)
+    print(predictor_var)
+    classification_model(model, df, predictor_var, outcome_var)
     for i in range(1,5):
         print('Pred: Huoneet:',i,'Talotiedot:',model.predict(i))
     lin()
-    
+
     # D.2.2 Ennuste Talotiedot <-- Kaupunginosa
     predictor_var = ['Kaupunginosa']
-    classification_model(model, df,predictor_var,outcome_var)
+    print(predictor_var)
+    classification_model(model, df,predictor_var, outcome_var)
     print('Pred: Klaukkala=2','Talotiedot:',model.predict(2))
     print('Pred: Nurmijarvi=1','Talotiedot:',model.predict(1))
+    lin()
 
     # D.2.3 Ennuste Talotiedot <-- Hissi
     predictor_var = ['Hissi']
-    classification_model(model, df,predictor_var,outcome_var)
+    print(predictor_var)
+    classification_model(model, df,predictor_var, outcome_var)
     print('Pred: Hissi on=1','Talotiedot:',model.predict(1))
     print('Pred: Hissi ei=0','Talotiedot:',model.predict(0))
+    lin()
 
     # D.2.4 Ennuste Talotiedot <-- m2
     predictor_var = ['m2']
-    classification_model(model, df,predictor_var,outcome_var)
+    print(predictor_var)
+    classification_model(model, df, predictor_var, outcome_var)
     for i in range(30,150,20):
         print('Pred: m2',i,'Talotiedot:',TalotiedotName[model.predict(i)])
+    lin()
 
-    # D.2.5 Ennuste Talotiedot <-- m2,Rv
+    # D.2.5 Ennuste Talotiedot <-- m2, Rv
     predictor_var = ['m2','Rv']
-    classification_model(model, df,predictor_var,outcome_var)
+    print(predictor_var)
+    classification_model(model, df, predictor_var, outcome_var)
+    lin()
+
+    # D.2.6 Ennuste Talotiedot <-- m2, Rv, huoneet
+    predictor_var = ['m2','Rv','Huoneet']
+    print(predictor_var)
+    classification_model(model, df, predictor_var, outcome_var)
+    lin()
+
+    input("\nPress Enter to continue")
+    print('-----------\n')
 
 
     # D.3 Logistic regression, Decision Tree, Random Forest
     lin()
-
     print('LogisticRegression')
     model = LogisticRegression()
     predictor_var = ['Huoneet','m2','Kaupunginosa','Rv','Hissi','Vh','Neliohinta']
@@ -278,10 +328,11 @@ if __name__ == '__main__':
     model = RandomForestClassifier(n_estimators=100)
     predictor_var = ['Huoneet','m2','Kaupunginosa','Rv','Hissi','Vh','Neliohinta']
     print(predictor_var)
-    classification_model(model, df,predictor_var,outcome_var)
+    classification_model(model, df, predictor_var, outcome_var)
 
     #Create a series with feature importances:
     featimp = pd.Series(model.feature_importances_, index=predictor_var).sort_values(ascending=False)
+    print('Feature importances:')
     print(featimp)
     lin()
 
@@ -300,6 +351,10 @@ if __name__ == '__main__':
     classification_model(model, df,predictor_var,outcome_var)
     lin()
 
+    input("\nPress Enter to continue")
+    print('-----------\n')
+
+
     print('For previous model, sample RFs parameters and see the CV score')
     i=0
     while i < 1: # 20
@@ -316,7 +371,7 @@ if __name__ == '__main__':
 
     # Best param found for now: 80 18 10 1: cv=85.259%, acc=89.877
     print('Best for now:')
-    ne,mss,md,mf=80, 18, 10, 1
+    ne, mss, md, mf = 80, 18, 10, 1
     print('RandomForestClassifier')
     model = RandomForestClassifier(n_estimators=ne,min_samples_split=mss,max_depth=md,max_features=mf)
     predictor_var = ['m2','Vh','Hissi','Rv']
@@ -328,6 +383,12 @@ if __name__ == '__main__':
     prediction=model.predict(features)
     print('Predict:',features,' Talotiedot:',TalotiedotName[prediction])
     lin()
+
+
+    input("\nPress Enter to continue")
+    print('-----------\n')
+
+
 
 
     # D.4 K-Means 
@@ -368,6 +429,10 @@ if __name__ == '__main__':
     predicted = k_means.predict(df[predictor_var].iloc[testRange,:])
     print('DF:',df[predictor_var][upperlimit+1:upperlimit+5])
     print('Predicted classes:',predicted[0:4])
+
+
+    input("\nPress Enter to continue")
+    print('-----------\n')
 
 
     # D.5 Dimensionality reduction 
