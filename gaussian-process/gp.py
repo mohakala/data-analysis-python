147f8a707bd6a45e7839bde1b7c642ec34c690e1
mikko.o.hakala@gmail.com <mikko.o.hakala@gmail.com>
2017-04-18 23:53:42 +0300

    study white kernel

--- gaussian-process/gp.py
+++ gaussian-process/gp.py
@@ -11,14 +11,23 @@ def plotGPRresults(gp, X, y, xtest=0, ytest_true=0, dy=0):
     """
     Plot results for the gp model (one-dimensional data X)
     dy = error in the training data
-    """
-    y_pred, sigma = gp.predict(xtest, return_std=True)
-    plt.figure()
+    X, y, dy = training data
     
-    # Predictions    
+    xtest = test data x-points
+    ytest_true = true function at xtest-points
+    """
+    
+    # Calculate the predictions
+    y_pred, sigma = gp.predict(xtest, return_std=True)
+
+
+    plt.figure()
+        
+    # Plot the predictions    
     plt.plot(xtest, y_pred, 'b-', label='predictions')
     plt.plot(xtest, y_pred - 2*sigma, 'b:')
     plt.plot(xtest, y_pred + 2*sigma, 'b:')
+
 
     # Plot the training data, with errorbar dy if it's available
     if(np.sum(dy) != 0):       
@@ -26,9 +35,11 @@ def plotGPRresults(gp, X, y, xtest=0, ytest_true=0, dy=0):
     else:
         plt.plot(X, y, 'r.', markersize=10, label='training data')
 
+
     # Plot the true function if available
     if(np.sum(ytest_true) != 0):  
         plt.plot(xtest, ytest_true, 'r:', label='true function')
+
     
     plt.xlabel('coordinate')
     plt.ylabel('energy')
@@ -43,52 +54,72 @@ def function(X):
 
 
 
-# A. Datapoints without noise
+# A. Datapoints, no noise
 
-# Training data
+# Training data A
 X = np.atleast_2d([1, 5, 6, 7, 8, 10]).T 
 y = function(X).ravel()
 
-# Another data
+# Training data B
 X1 = np.linspace(-4, 5, 10).reshape(-1,1)
-y1 = np.array([1, 1.8, 3.2, 3.9, 4.5, 4.7, 3.3, 2.9, 2.0, 1.3]).ravel()
+y1 = np.array([1, 1.8, 3.2, 3.9, 4.5, 5.1, 3.8, 3.6, 3.5, 2.1]).ravel()
 
+# Choose which training data to use
 X = X1
 y = y1
 
-print(X)
-print(y)                 
+print('Xtrain', X)
+print('ytrain', y)                 
  
                  
 
 from sklearn.gaussian_process import GaussianProcessRegressor
-from sklearn.gaussian_process.kernels import RBF, ConstantKernel as C
+from sklearn.gaussian_process.kernels import RBF, ConstantKernel as C, WhiteKernel
 
 
-# Choose kernel
+
+
+
+# Version 1 with target's known noise level
 kernel = C(1.0) * RBF(10, (1e-1, 1e2))
-
-
-print('Kernel information:')
-testC = C(1.0)
-testRBF = RBF(10, (1e-1, 1e2))
-print(testC.get_params())
-print(testRBF.get_params())
-
-# Target's known noise level
 alpha = 0.1
+gp = GaussianProcessRegressor(kernel=kernel, alpha=alpha, n_restarts_optimizer=10)
+
+
+# Print kernel information
+if(False):
+    print('Kernel information:')
+    testC = C(1.0)
+    testRBF = RBF(10, (1e-1, 1e2))
+    print(testC.get_params())
+    print(testRBF.get_params())
+
 
 # Fit the hyperparameters
-gp = GaussianProcessRegressor(kernel=kernel, alpha=alpha, n_restarts_optimizer=10)
 gp.fit(X, y)
 print('\nOptimized kernel:', gp.kernel_)
+print('Log marginal likelihood:', gp.log_marginal_likelihood_value_)
+    
+    
+
+# Version 2 with White Kernel
+kernel = C(1.0) * RBF(1, (1e-1, 1e2)) + WhiteKernel(noise_level=1e-5, noise_level_bounds=(1e-10, 1e+1))
+gp = GaussianProcessRegressor(kernel=kernel, alpha=0.0, n_restarts_optimizer=10)
+
+# Fit the hyperparameters
+gp.fit(X, y)
+print('\nOptimized kernel:', gp.kernel_)
+print('Log marginal likelihood:', gp.log_marginal_likelihood_value_)
 
 
-# Plot predictions 
+
+
+
+# Plot predictions, data A 
 xtest = np.atleast_2d(np.linspace(0, 10, 100)).T
 ytest_true = function(xtest)
 
-# Plot predictions, data 2
+# Plot predictions, data B
 xtest = np.atleast_2d(np.linspace(-5, 5, 50)).T
 
 
