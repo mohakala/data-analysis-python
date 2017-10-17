53319db868b4619349ab7cb534c8e5fb7305124c
mikko.o.hakala@gmail.com <mikko.o.hakala@gmail.com>
2017-10-18 00:11:47 +0300

    add hidden markov model

--- markov-chain/markovchain.py
+++ markov-chain/markovchain.py
@@ -20,13 +20,15 @@ class markov(object):
         """
         if tm is not None:
             self.tm = tm
-        self.check_tm()
+            self.check_tm()
 
 
     def check_tm(self):
         """
+        Check that the matrix is a square matrix
         Check that all the rows of transition matrix sum to 1.0
         """
+        assert self.tm.shape[0]==self.tm.shape[1], "not a square matrix"
         l = len(self.tm)
         for i in range(l):
             assert np.sum(self.tm[i, :])==1.0, "row %i in transition matrix does not sum to 1.0" % i
@@ -57,6 +59,7 @@ class markov(object):
             norm2 = np.linalg.norm(state)
             # print('i, state, norm1, norm2:', i, state, norm1, norm2)
             if(np.abs(norm2-norm1)/norm1 < epsilon):
+                print('>convergence in ', i, 'steps')
                 return(state)
             norm1 = norm2
         print('i=100 reached, no convergence in norm')
@@ -70,7 +73,7 @@ class markov(object):
         print('determinant:', det)
 
 
-def main():
+def normal_markovchain():
     # Transition matrix
     # [A to A, A to B]
     # [B to A, B to B]
@@ -93,7 +96,7 @@ def main():
             [0.0, 0.00, 1.0, 0.0],
             [0.0, 0.0, 0.0, 1.0]
             ])
-
+    
     state = np.array([0.60, 0.40, 0.0, 0.0])
 
     # Example 2b
@@ -101,7 +104,13 @@ def main():
 
     # Example 2c
     state = np.array([0.0, 1.0, 0.0, 0.0])
-    
+
+    # Example 3: rain, no rain
+    tm = np.array([
+            [0.5, 0.5],
+            [0.05, 0.95]
+            ])
+    state = np.array([0.0, 1.0])
 
     mk = markov(tm)
     
@@ -115,11 +124,82 @@ def main():
     # Study the transition matrix
     # mk.matrixprop(tm)
 
-
  
+def hidden_markovchain():
+#    from hmmlearn import hmm
+    from hmmlearn.hmm import GaussianHMM
+    
+    startprob = np.array([0.6, 0.3, 0.1, 0.0])
+    transmat = np.array([[0.7, 0.2, 0.0, 0.1],
+                     [0.3, 0.5, 0.2, 0.0],
+                     [0.0, 0.3, 0.5, 0.2],
+                     [0.2, 0.0, 0.2, 0.6]])
+    # 2-dimensional
+    means2 = np.array([[0.0,  0.0],
+                  [0.0, 11.0],
+                  [9.0, 10.0],
+                  [11.0, -1.0]])
+    
+    covars2 = .5 * np.tile(np.identity(2), (4, 1, 1))
+
+    # 1-dimensional
+    means=np.array([[0.0],
+                  [5.0],
+                  [50.0],
+                  [100.0]])
+    covars = .5 * np.tile(np.identity(1), (4, 1, 1))
+
+    
+
+#    model = hmm.GaussianHMM(n_components=4, covariance_type="full")
+    model = GaussianHMM(n_components=4, covariance_type="full")
+
+    # Set the parameters
+    model.startprob_ = startprob
+    model.transmat_ = transmat
+    model.means_ = means
+    model.covars_ = covars
+
+    # Generate samples
+    X, Z = model.sample(15)
+    print(X, Z)
+
+
+
+    if(False):    
+        plt.figure()
+        plt.plot(X)
+        plt.plot(Z)
+        plt.show()
+
+
+    # Estimate optimal sequence of hidden states
+    remodel = GaussianHMM(n_components=4, covariance_type="full", n_iter=100)
+    remodel.fit(X)  
+    print(remodel.score)
+    Z2 = remodel.predict(X)
+    print(Z2)
+    print(remodel.monitor_)
+    print('Converged:', remodel.monitor_.converged)
+    
+    print("Predictions:")
+    for i in range(remodel.n_components):
+        print("mean = ", remodel.means_[i])
+        print("var = ", np.diag(remodel.covars_[i]))
+
+    
+    
+
+
+def main():    
+    if(False):
+        normal_markovchain()
+        
+    if(True):
+        hidden_markovchain()
     
     print('\nDone')    
-    
+
     
 if __name__ == '__main__':
     main()
