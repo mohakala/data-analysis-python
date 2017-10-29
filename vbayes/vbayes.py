9ced0f3ea96d1b5e66ff800e209571daff837685
mikko.o.hakala@gmail.com <mikko.o.hakala@gmail.com>
2017-10-30 00:28:37 +0200

    study normal and binomial examples

--- vbayes/vbayes.py
+++ vbayes/vbayes.py
@@ -11,55 +11,351 @@ cwd = os.getcwd()
 print(cwd)
 
 import  numpy as np
-
-# Two 2D-Gaussians at (0,0) and (2,2)
-N = 500; D = 2
-data = np.random.randn(N, D)
-data [:200 ,:] += 2*np.ones(D)
-
-# Maximum number of clusters
-K = 5
-
-
+import  bayespy.plot as bpplt
 from  bayespy  import  nodes
 
-# Initialize the means mu of the unknown clusters
-mu = nodes.Gaussian(np.zeros(D), 0.01* np.identity(D), plates =(K,))
-print(mu)
-print("np.identity(D):", np.identity(D))
 
-# Initialize the precisions Lambda of the unknown clusters
-#   Wishart distribution for conjugate prior
-Lambda = nodes.Wishart(D, D*np.identity(D), plates =(K,))
-print(Lambda)
+def defective():
+    """ 
+    Binomial data 
+    From Bayesian Ideas and Data Analysis
+    """
+    import warnings
+    warnings.filterwarnings('ignore', category=RuntimeWarning)
 
-# Cluster probabilities: Dirichlet prior
-alpha = nodes.Dirichlet (0.01* np.ones(K))
+    # Relevant distribution: Binomial distribution 
+    #   there are x% defective parts in the production process
 
-# Cluster assignments 
-z = nodes.Categorical(alpha, plates =(N,))
+    # Prior belief: 
+    #  propoortion of scrap is in the range 5% to 15%
+    # This can be approximated by the following Beta distribution 
+    theta = nodes.Beta([12.05, 116.06])
+    # print(theta.pdf(0.1)) # Probability of theta being 0.1
 
-# Observations from Gaussian mixture distribution
-y = nodes.Mixture(z, nodes.Gaussian , mu , Lambda)
+    # Plot the prior distibution for theta
+    import bayespy.plot as bpplt
+    bpplt.pyplot.figure()
+    bpplt.pdf(theta, np.linspace(0.01, 0.150, num=300))
+    bpplt.pyplot.title(' ')    
 
-# Observations (data)
-y.observe(data)
+    # Number of parts examined
+    n_trials = 2430
 
-# Variational Bayesian inference engine
-from  bayespy.inference  import  VB
-Q = VB(y, mu , z, Lambda , alpha)
+    # The number of defective parts has binomial distribution
+    y = nodes.Binomial(n_trials, theta)
 
-# Random initial assignments
-z.initialize_from_random ()
+    # Observed n=219 defective parts
+    data=219
+    y.observe(data)
 
-Q.update(repeat =200)
+    # Update the prior to get the posterior distribution
+    theta.update()
 
-import  bayespy.plot as bpplt
-bpplt.gaussian_mixture_2d(y, alpha=alpha)
-bpplt.pyplot.show()
+    # Plot the theta posterior distribution
+    bpplt.pyplot.figure()
+    bpplt.pdf(theta, np.linspace(0.01, 0.150, num=300))
+    bpplt.pyplot.title(' ')    
 
-# Print the parameters of the approximate posterior distributions
-print(alpha)
+    if(False): 
+        for i in range(1):
+            data=500
+            y.observe(data)
+            theta.update()
+        # Plot the new theta distribution
+        bpplt.pyplot.figure()
+        bpplt.pdf(theta, np.linspace(0.01, 0.150, num=300))
+        bpplt.pyplot.title(' ')    
+    
+    
+    
+def example_from_article():
+    """ Example from the paper """
 
+    # Model
+    from bayespy.nodes import GaussianARD
+    mu = GaussianARD(0, 1e-6)
+    from bayespy.nodes import Gamma
+    tau = Gamma(1e-6, 1e-6)
+
+    y = GaussianARD(mu, tau, plates=(12,))
+    
+    data = np.array([4.5, 3.9, 6.3, 5.6, 4.9, 2.8, 7.4, 6.1, 4.8, 2.1])
+
+    # Other data to test
+    data_zinc = np.array([4.20, 4.36, 4.11, 3.96, 
+                     5.63, 4.50, 5.64, 4.38, 
+                     4.45, 3.67, 5.26, 4.66])
+
+    
+    y.observe(data_zinc)
+    
+    # Inference engine
+    from bayespy.inference import VB
+    Q = VB(y, mu, tau)
+    print('get_parameters:', Q.get_parameters(y, mu, tau))
+    print(Q.has_converged())
+
+    Q.update(repeat=100)
+    
+    # print('mu:', Q[mu]) # does not work
+    print('tau:', Q[tau])
+    print('get_moments, mu:', mu.get_moments())
+    #print('get_moments:', y.get_moments())
+    print('get_moments, tau:', tau.get_moments())
+
+    
+    # Examine the posterior approximation
+    import bayespy.plot as bpplt
+    bpplt.pyplot.figure()
+    bpplt.pdf(Q[mu], np.linspace(0, 10, num=300))
+    bpplt.pyplot.title('PDF of mu')    
+    
+    bpplt.pyplot.figure()
+    bpplt.pdf(Q[tau], np.linspace(1e-6, 7, num=300))
+    bpplt.pyplot.title('PDF of tau')    
+    
+    
+    
+
+    
+
+def zinc():
+    """ 
+    Zinc content data (normal data)
+    From Bayesian Ideas and Data Analysis
+    """
+    data = np.array([4.20, 4.36, 4.11, 3.96, 
+                     5.63, 4.50, 5.64, 4.38, 
+                     4.45, 3.67, 5.26, 4.66])
+    
+    ## Model
+    X = np.ones(len(data)).reshape(-1, 1)
+    print(X)
+
+    # Prior mean
+    from bayespy.nodes import GaussianARD
+    B = GaussianARD(4.75, 0.0163, shape=(1,))
+    print('B:', B)
+
+    # BX
+    from bayespy.nodes import SumMultiply
+    F = SumMultiply('i,i', B, X)
+    print('F:', F)
+
+    # Prior distribution for tau = sigma^-2
+    from bayespy.nodes import Gamma
+    tau = Gamma(1e-3, 1e-3)
+    print('tau:', tau)
+
+
+    # Noisy observations
+    Y = GaussianARD(F, tau)
+    print('Y:', Y)
+
+
+    ## Inference
+    
+    Y.observe(data)
+
+    # PDF of Y (?)
+    import bayespy.plot as bpplt
+    bpplt.pyplot.figure()
+    bpplt.pdf(Y, np.linspace(1e-6,8,12), color='k')
+    #bpplt.pdf(Y, data, color='k')
+
+    bpplt.pyplot.title('PDF of Y (?)')
+
+    # VB inference engine by using stochastic nodes
+    from bayespy.inference import VB
+    Q = VB(Y, B, tau)
+    print('get_moments, B:', B.get_moments())
+    Q.update(repeat=1000)
+    print('After engine:')
+    print('B:', B)
+    print('get_moments, B:', B.get_moments())
+    print('tau:', tau)
+    
+    
+    ## Show results
+    
+    # New inputs
+    xh = np.linspace(-5, 15, 100)
+    Xh = np.vstack([np.ones(len(xh))]).T
+    Fh = SumMultiply('i,i', B, Xh)
+
+    # Prediction
+    import bayespy.plot as bpplt
+    bpplt.pyplot.figure()
+    bpplt.plot(Fh, x=xh, scale=1) # Prediction with scale STD's
+    bpplt.plot(data, x=np.arange(len(data)), color='r', marker='x', linestyle='None') # Samples
+
+    # PDF of tau
+    bpplt.pyplot.figure()
+    bpplt.pdf(Q[tau], np.linspace(1e-6,8,100), color='k')
+    bpplt.pyplot.title('PDF of tau')
+
+
+
+    
+
+
+def linreg():
+    """ http://bayespy.org/examples/regression.html """
+
+    ## Data
+    
+    k = 2 # slope
+    c = 5 # bias
+    s = 2 # noise standard deviation
+
+    x = np.arange(10)
+    y = k*x + c + s*np.random.randn(10)
+
+    ## Model
+    
+    X = np.vstack([x, np.ones(len(x))]).T
+    
+    # Prior mean
+    from bayespy.nodes import GaussianARD
+    B = GaussianARD(0, 1e-6, shape=(2,))
+    print('B:', B)
+
+    # BX
+    from bayespy.nodes import SumMultiply
+    F = SumMultiply('i,i', B, X)
+    print('F:', F)
+
+    # Prior distribution for tau = sigma^-2
+    from bayespy.nodes import Gamma
+    tau = Gamma(1e-3, 1e-3)
+    print('tau:', tau)
+
+    # Noisy observations
+    Y = GaussianARD(F, tau)
+    print('Y:', Y)
+
+
+    ## Inference
+    
+    Y.observe(y)
+
+    # VB inference engine by using stochastic nodes
+    from bayespy.inference import VB
+    Q = VB(Y, B, tau)
+    Q.update(repeat=1000)
+    print('B:', B)
+    print('tau:', tau)
+    
+    
+    ## Show results
+    
+    # Prediction
+    
+    # New inputs
+    xh = np.linspace(-5, 15, 100)
+    Xh = np.vstack([xh, np.ones(len(xh))]).T
+    Fh = SumMultiply('i,i', B, Xh)
+
+    import bayespy.plot as bpplt
+    bpplt.pyplot.figure()
+    bpplt.plot(Fh, x=xh, scale=2) # Prediction with 2 STD's
+    bpplt.plot(y, x=x, color='r', marker='x', linestyle='None') # Samples
+    bpplt.plot(k*xh+c, x=xh, color='r') # True function
+
+
+    # Plot PDF of noice parameter tau
+    bpplt.pyplot.figure()
+    bpplt.pdf(Q[tau], np.linspace(1e-6,1,100), color='k')
+    # Alternative: Just bpplt.pdf(tau, np.linspace...)
+    bpplt.pyplot.axvline(s**(-2), color='r');
+    bpplt.pyplot.title('PDF of tau')
+
+
+    # Plot regression parameters and the true values
+    bpplt.pyplot.figure();
+    bpplt.contour(B, np.linspace(1,3,1000), np.linspace(1,9,1000),
+                  n=10, colors='k');
+    bpplt.plot(c, x=k, color='r', marker='x', linestyle='None',
+               markersize=10, markeredgewidth=2)
+    bpplt.pyplot.xlabel(r'$k$');
+    bpplt.pyplot.ylabel(r'$c$');
+    bpplt.pyplot.title('Regression parameters')
+
+
+
+    
+
+    
+def two_gaussians():
+    """ Two 2D-Gaussians at (0,0) and (2,2) """
+    N = 500; D = 2
+    data = np.random.randn(N, D)
+    data [:200 ,:] += 2*np.ones(D)
+
+    # Maximum number of clusters
+    K = 5
+
+    # Initialize the means mu of the unknown clusters
+    mu = nodes.Gaussian(np.zeros(D), 0.01* np.identity(D), plates =(K,))
+    print(mu)
+    print("np.identity(D):", np.identity(D))
+
+    # Initialize the precisions Lambda of the unknown clusters
+    #   Wishart distribution for conjugate prior
+    Lambda = nodes.Wishart(D, D*np.identity(D), plates =(K,))
+    print(Lambda)
+    
+    # Cluster probabilities: Dirichlet prior
+    alpha = nodes.Dirichlet (0.01* np.ones(K))
+    
+    # Cluster assignments 
+    z = nodes.Categorical(alpha, plates =(N,))
+
+    # Observations from Gaussian mixture distribution
+    y = nodes.Mixture(z, nodes.Gaussian , mu , Lambda)
+
+    # Observations (data)
+    y.observe(data)
+
+    # Variational Bayesian inference engine
+    from  bayespy.inference  import  VB
+    Q = VB(y, mu , z, Lambda , alpha)
+
+    # Random initial assignments
+    z.initialize_from_random ()
+
+    Q.update(repeat =200)
+
+    bpplt.gaussian_mixture_2d(y, alpha=alpha)
+    bpplt.pyplot.show()
+
+    # Print the parameters of the approximate posterior distributions
+    print(alpha)
+
+
+
+def main():
+
+    if(True):
+        defective()
+        
+    if(False):
+        example_from_article()
+       
+    if(False):
+        zinc()
+
+    if(False):
+        linreg()
+
+    if(False):
+        two_gaussians()
+
+
+
+    print("Done")
+    
+if __name__ == '__main__':
+    main()
 
 
